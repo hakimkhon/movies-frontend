fc26add6864187bf8d9e51750ac1ea3d62d09bdd
hakimkhon <hakimkhon94@gmail.com>
2022-06-16 18:08:19 +0500

    oxshamadi

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, {Component} from 'react'
+import axios from 'axios'
+class App extends Component{
+  state = {
+    films:[]
+  };
+  componentDidMount(){
+    this.getFilms();
+  }
+  getFilms(){
+    axios
+    .get('http://127.0.0.1:8000/api/v/')
+    .then(res=>{
+      this.setState({films:res.data})
+    })
+    .catch(err=>{
+      console.log(err)
+    })
+  }
+  render(){
+    return(
+      <div>
+        {this.state.list.map(item =>(
+          <div class={item.id}>
+            <h1>{item.title}</h1>
+            <h2>{item.rejisor}</h2>
+            <p>{item.about_film}</p>
+            <h3>{item.pochta}</h3>
+          </div>
+        ))}
+      </div>
+    )
+  }
 }
-
 export default App;
